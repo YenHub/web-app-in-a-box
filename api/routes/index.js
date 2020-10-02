8aee71eedff0c4a092215156cf4728067b265665
YenHub <gilkes@gmx.com>
2020-10-02 18:43:42 +0100

    🚀 Setup 403 on index, change welcome message to status page

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.status(403);
+    res.render('index', { title: 'Nothing to see here 😁' });
 });
 
 module.exports = router;
