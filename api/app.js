456c1d66175ddcd0b936f8a0a328cf16d457ab6d
YenHub <gilkes@gmx.com>
2020-10-23 19:50:37 +0100

    🔨 Add dotenv config call for standalone api

--- api/app.js
+++ api/app.js
@@ -9,6 +9,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var testRouter = require('./routes/testAPI');
 
+require('dotenv').config();
+
 var app = express();
 
 // view engine setup
