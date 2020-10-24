ff1b2220433438398d2c565d2e68297fa6229829
YenHub <gilkes@gmx.com>
2020-10-24 12:41:46 +0100

    ❄ Let it snow... Christmas snow fall wrapper for 2020

--- client/src/pages/Generic/Wrapper.js
+++ client/src/pages/Generic/Wrapper.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../../App.css';
+import Snowfall from 'react-snowfall';
 
 function Wrapper(app) {
 
     return (
         <div className="App">
+            <Snowfall />
             <div className="App-header">
                 {app}
             </div>
